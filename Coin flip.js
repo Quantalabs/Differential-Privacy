2f6b97308ca97d2a8a201e88abb78eed2451a578
Quant <55121845+Quantalabs@users.noreply.github.com>
2020-02-21 21:00:48 -0800

    Finished!

--- Coin flip.js
+++ Coin flip.js
@@ -1,15 +1,24 @@
+var e = [true, false, true, true];
 var flip = function (dataArray) {
-  var a = dataArray;
-  for(var i = 0; i < a; i++) {
-    var b = Math.random(0,1);
-    var c = Math.round(b);
-    var d;
-    if(c = 0) {
-      a[i] = a[i];
+    var a = dataArray;
+    for(var i = 0; i < a.length; i++) {
+        var b = Math.random(0,1);
+        var c = Math.round(b);
+        if(c === 0) {
+            a[i] = true;
+        }
+        if(c === 1) {
+            var b1 = Math.random(0,1);
+            var c1 = Math.round(b1);
+            if(c1 === 0) {
+                a[i] = true;
+            }
+            if(c1 === 1) {
+                a[i] = false;
+            }
+        }
     }
-    else {
-      a[i] = false;
-    }
-  }
-  return a;
+    dataArray = a;
 }
+flip(e);
+console.log(e);
